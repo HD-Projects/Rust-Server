1d1e8917871bd2525073fedb1c03fe37d1a6b5d9
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-10-26 10:06:37 -0600

    Trying azure cloud services

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,24 @@ fn index() -> &'static str {
     "Hello, world!"
 }
 
+#[get("/<post>")]
+fn post_int(post: i16) -> String{
+	format!("int")
+
+}
+
+#[get("/<post>", rank = 2)]
+fn post_str(post: String) -> String{
+	let header = "Header stuff";
+	let author = "ad101-lab";
+	let content = "Blog post content!";
+	let footer = "Footer stuff";
+	return format!("{} \nBlog post: \n author: {} \n Content: {} \n{} Name of page: {}", header, author, content, footer, name);
+}
+
 fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+    	.mount("/", routes![index])
+    	.mount("/blog", routes![post_int, post_str])
+    	.launch();
 }
